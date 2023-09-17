8ab2d87df2758bdaad035209e917f882911abf4f
Chengwei <oncwnuI4a2J28cBF1X7DHpYdxjG4@git.weixin.qq.com>
2023-09-17 17:57:09 +0800

    Info updated

--- yitboo.js
+++ yitboo.js
@@ -34,7 +34,7 @@ function switchLanguage(language) {
 function updateContent() {
     if (currentLanguage === 'en') {
         welcomeMessage.innerHTML = 'Welcome to YitBoo!';
-        welcomeDesc.innerHTML = 'YitBoo is the first <strong>Internet-based fabric trading platform</strong> in Foshan, Guangdong. It\'s free to use and is dedicated to providing efficient and convenient services for both fabric suppliers and buyers.Our goal is to make it easy for you to find and sell fabrics, fostering mutually beneficial transactions.Additionally, we offer the latest updates and news from the fabric industry, allowing you to stay ahead in this rapidly evolving field.<br><br><strong>@Foshan YitBoo Tech Co., Ltd</strong>';
+        welcomeDesc.innerHTML = 'YitBoo is the first <strong>online fabric trading platform</strong> in Foshan, Guangdong. It\'s free to use and is dedicated to providing efficient and convenient services for both fabric suppliers and buyers. Our goal is to make it easy for you to find and sell fabrics, fostering mutually beneficial transactions. Additionally, we offer the latest updates and news from the fabric industry, allowing you to stay ahead in this rapidly evolving field.<br><br><strong>Follow us on Facebook: <a href="https://www.facebook.com/profile.php?id=61551196983388">@YitBoo Tech Co., Ltd</a></strong>';
 
         buyMessage.innerHTML = 'YitBoo, first choice for procurement.';
         buyDesc.innerHTML = 'YitBoo, with over two decades of experience in the fabric industry, has built a reputation for integrity and has accumulated extensive market resources. Our extensive supplier network ensures comprehensive support and convenience, making your procurement process simpler and more reliable.<br><br>As a dedicated fabric sales platform, YitBoo is committed to providing you with a rich and diverse online fabric repository. We aim to help you quickly find the fabric you need, ultimately enhancing your business efficiency, with:';
@@ -52,7 +52,7 @@ function updateContent() {
         teamDesc.innerHTML = 'The core members of the YitBoo team graduated from renowned institutions both domestically and internationally, including Sun Yat-sen University, Université Paris-Saclay, and Aalto University. They have previously worked at prominent internet companies such as Alibaba, Tencent, and NetEase, accumulating years of experience in fabric trading, platform operation, and digital marketing. Our professional team is here to support your business expansion.<br><br>Furthermore, our network of partners spans major textile markets across China, including China Textile City, Zhujiang International Textile City, China Textile Garment City, Shishi International Textile City, and Jiangnan International Fashion City, among others.';
 
         contactMessage.innerHTML = 'Contact Us';
-        contactDesc.innerHTML = 'WhatsApp: +86 156 2277 2205<br><br>Email: machengwei@yitboo.com<br><br>Tel.: +86 0757 8579 8189<br><br>Address: G30, Pingdi Guoji Niuzai Cheng, YanBu Town, Nanhai District, Foshan City, Guangdong Province, China.';
+        contactDesc.innerHTML = 'WhatsApp: +86 156 2277 2205<br><br>Email: business@yitboo.com<br><br>Tel.: +86 0757 8579 8189<br><br>Address: G30, Pingdi Guoji Niuzai Cheng, YanBu Town, Nanhai District, Foshan City, Guangdong Province, China.';
         contactDesc2.innerHTML = 'If you have WeChat, scan these QR codes to know more about us!'
 
     } else if (currentLanguage === 'zh') {
@@ -75,7 +75,7 @@ function updateContent() {
         teamDesc.innerHTML = '亿布团队核心成员毕业于中山大学、巴黎萨克雷大学、芬兰阿尔托大学等国内外知名院校，曾就职阿里巴巴、腾讯、网易等知名互联网企业，拥有多年面料交易、平台运营、数字营销经验，专业团队支持您的业务拓展。<br><br>我们的合作伙伴也覆盖全国各大纺织市场，包括中国轻纺城、珠江国际纺织城、中纺华丽、石狮国际轻纺城及江南国际服装城等。';
 
         contactMessage.innerHTML = '联系我们';
-        contactDesc.innerHTML = '电话：0757-85798189<br>邮箱：machengwei@yitboo.com<br>地址：广东省佛山市南海区盐步平地国际牛仔城G30号';
+        contactDesc.innerHTML = '电话：0757-85798189<br>邮箱：business@yitboo.com<br>地址：广东省佛山市南海区盐步平地国际牛仔城G30号';
         contactDesc2.innerHTML = '公众号：<strong>@亿布信息 YitBoo</strong><br> 小程序：<strong>@亿布信息丨布料交易布料库</strong> <br>小红书：<strong>@亿布信息-在线布料库</strong><br> 抖音号：<strong>@小亿说布</strong>';
     }
 }
